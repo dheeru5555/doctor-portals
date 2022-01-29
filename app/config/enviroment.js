5b04a0f70a1303c6d8464da20eaf80aec5dadb66
dheeru5555 <dysengg@gmail.com>
2022-01-29 16:43:11 +0530

    Chnages for URL in doc App

--- app/config/enviroment.js
+++ app/config/enviroment.js
@@ -1,4 +1,6 @@
 export default {
   // api_url: 'http://13.126.160.234/oaarogya-admin-test/api/',
-  api_url: 'https://oaarogyabetaportal.mirakidigital.in/api/',
+  // api_url: 'https://oaarogyabetaportal.mirakidigital.in/api/',
+  api_url: 'http://103.86.177.208/oaarogya-admin-backend/api/',
+
 };
--- app/helpers/api.js
+++ app/helpers/api.js
@@ -4,40 +4,38 @@ import React from 'react';
 import history from '../utils/history';
 
 class API extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       // coreUri: 'http://13.126.160.234/oaarogya-admin-test/api/doctor/',
       // mUri: 'http://13.126.160.234/oaarogya-admin-test/api/masterdata',
-      coreUri: 'https://oaarogyabetaportal.mirakidigital.in/api/doctor/',
-      mUri: 'https://oaarogyabetaportal.mirakidigital.in/api/masterdata',
-    }
-
+      coreUri: 'http://103.86.177.208/oaarogya-admin-backend/api/doctor/',
+      mUri: 'http://103.86.177.208/oaarogya-admin-backend/api/masterdata',
+    };
   }
 
   CORE_URI = () => {
-    const {coreUri} = this.state;
+    const { coreUri } = this.state;
     return axios.create({
       baseURL: coreUri,
     });
   }
 
   ACCOUNTS_URI = () => {
-    const {coreUri} = this.state;
+    const { coreUri } = this.state;
 
-    const userInfoInLocalStorage = localStorage.getItem("userInfo");
+    const userInfoInLocalStorage = localStorage.getItem('userInfo');
     const parsedUserInfo = JSON.parse(userInfoInLocalStorage);
-  
-    let bearerToken = "";
-  
-    if ((parsedUserInfo !== null) && 
-      parsedUserInfo.hashedKey && 
-      (parsedUserInfo.hashedKey.length > 0)) {
+
+    let bearerToken = '';
+
+    if ((parsedUserInfo !== null)
+      && parsedUserInfo.hashedKey
+      && (parsedUserInfo.hashedKey.length > 0)) {
       bearerToken = parsedUserInfo.hashedKey;
     } else {
-      history.push("/")
+      history.push('/');
     }
 
     return axios.create({
@@ -47,16 +45,16 @@ class API extends React.Component {
   }
 
   MASTER_URI = () => {
-    const {mUri} = this.state;
+    const { mUri } = this.state;
 
-    const userInfoInLocalStorage = localStorage.getItem("userInfo");
+    const userInfoInLocalStorage = localStorage.getItem('userInfo');
     const parsedUserInfo = JSON.parse(userInfoInLocalStorage);
-  
-    let bearerToken = "";
 
-    if ((parsedUserInfo !== null) && 
-      parsedUserInfo.hashedKey && 
-      (parsedUserInfo.hashedKey.length > 0)) {
+    let bearerToken = '';
+
+    if ((parsedUserInfo !== null)
+      && parsedUserInfo.hashedKey
+      && (parsedUserInfo.hashedKey.length > 0)) {
       bearerToken = parsedUserInfo.hashedKey;
     }
 
@@ -65,7 +63,6 @@ class API extends React.Component {
       headers: { Authorization: `Bearer ${bearerToken}` }
     });
   }
-
 }
 
 export default API;
